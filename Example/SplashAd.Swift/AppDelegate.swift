34d5299788f9ae4d32b52f30d0193f081680bea7
tomato <wujiafengwork@gmail.com>
2020-02-26 13:12:56 +0800

    feat: 设置 swiftlint

--- Example/SplashAd.Swift/AppDelegate.swift
+++ Example/SplashAd.Swift/AppDelegate.swift
@@ -17,8 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = .gray
         window?.makeKeyAndVisible()
@@ -72,7 +71,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
